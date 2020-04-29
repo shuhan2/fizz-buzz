13123516b77e236b77a2ae234bad5b4b73dfbab3
qianwu2 <qianwu2@ap.xom.com>
2020-04-29 20:21:54 +0800

    fizzbuzz [qian] should not ignore rule for multiple 3 when transform given number contains 5

--- src/main/java/Contain5Rule.java
+++ src/main/java/Contain5Rule.java
@@ -1,11 +1,4 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class Contain5Rule implements Rule {
-
-  private List<Rule> rules = Arrays.asList(new Multiple5Rule(),
-                                           new Multiple7Rule());
 
   @Override
   public boolean isMatchRule(int number) {
@@ -14,6 +7,6 @@ public class Contain5Rule implements Rule {
 
   @Override
   public String getReturnForRule(int number) {
-    return rules.stream().map(rule -> rule.getReturnForRule(number)).collect(Collectors.joining());
+    return new DefaultRule().getReturnForRule(number);
   }
 }
