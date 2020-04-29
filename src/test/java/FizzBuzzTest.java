12089f022fa0c7d37dede732bbea554a980bdc9a
qianwu2 <qianwu2@ap.xom.com>
2020-04-29 17:15:10 +0800

    fizzbuzz [qian] should return 'Buzz' when transform given 10.

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -8,7 +8,8 @@ class FizzBuzzTest {
   @ParameterizedTest
   @CsvSource({
       "'1', 1, 'should return original string value when transform given no special number'",
-      "'Fizz', 6, 'should return Fizz when transform given 6'"
+      "'Fizz', 6, 'should return Fizz when transform given 6'",
+      "'Buzz', 10, 'should return Buzz when transform given 10'"
   })
   void should_return_1_when_transform_given_1(String output, int number) {
     FizzBuzz fizzBuzz = new FizzBuzz();
